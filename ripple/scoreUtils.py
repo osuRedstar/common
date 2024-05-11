5dd46f8f85cefcc5f6bbc42469774dc6dd85a0ef
전강현 <aniren@debian.moe>
2024-05-12 01:08:55 +0900

    readableMods() 함수 통합
    generalUtils.py --> ripple\soreUtils.py

--- ripple/scoreUtils.py
+++ ripple/scoreUtils.py
@@ -34,35 +34,45 @@ def readableGameMode(gameMode):
 	else:
 		return "mania"
 
-def readableMods(m):
+def readableMods(__mods):
 	"""
 	Return a string with readable std mods.
 	Used to convert a mods number for oppai
 
-	:param m: mods bitwise number
+	:param __mods: mods bitwise number
 	:return: readable mods string, eg HDDT
 	"""
 	r = ""
-	if m == 0:
-		return "nomod"
-	if m & mods.NOFAIL > 0:
-		r += "NF"
-	if m & mods.EASY > 0:
-		r += "EZ"
-	if m & mods.HIDDEN > 0:
-		r += "HD"
-	if m & mods.HARDROCK > 0:
-		r += "HR"
-	if m & mods.DOUBLETIME > 0:
-		r += "DT"
-	if m & mods.HALFTIME > 0:
-		r += "HT"
-	if m & mods.FLASHLIGHT > 0:
-		r += "FL"
-	if m & mods.SPUNOUT > 0:
-		r += "SO"
-	if m & mods.TOUCHSCREEN > 0:
-		r += "TD"
-	if m & mods.RELAX > 0:
-		r += "RX"
+	if __mods == mods.NOMOD: return r
+	if __mods & mods.NOFAIL > 0: r += "NF"
+	if __mods & mods.EASY > 0: r += "EZ"
+	if __mods & mods.TOUCHSCREEN > 0: r += "TD"
+	if __mods & mods.HIDDEN > 0: r += "HD"
+	if __mods & mods.HARDROCK > 0: r += "HR"
+	if __mods & mods.SUDDENDEATH > 0: r += "SD"
+	if __mods & mods.DOUBLETIME > 0: r += "DT"
+	if __mods & mods.RELAX > 0: r += "RX"
+	if __mods & mods.HALFTIME > 0: r += "HT"
+	if __mods & mods.NIGHTCORE > 0 or __mods & 576 > 0: r = r.replace("DT", "NC") #576 = DT, NC
+	if __mods & mods.FLASHLIGHT > 0: r += "FL"
+	if __mods & mods.AUTOPLAY > 0: r += "AU(AUTO)"
+	if __mods & mods.SPUNOUT > 0: r += "SO"
+	if __mods & mods.RELAX2 > 0: r += "AP"
+	if __mods & mods.PERFECT > 0 or __mods & 16416 > 0: r = r.replace("SD", "PF") #16416 = SD, PF
+	if __mods & mods.KEY4 > 0: r += "K4"
+	if __mods & mods.KEY5 > 0: r += "K5"
+	if __mods & mods.KEY6 > 0: r += "K6"
+	if __mods & mods.KEY7 > 0: r += "K7"
+	if __mods & mods.KEY8 > 0: r += "K8"
+	if __mods & mods.KEYMOD > 0: r += "KEYMOD" #?
+	if __mods & mods.FADEIN > 0: r += "FI"
+	if __mods & mods.RANDOM > 0: r += "RD"
+	if __mods & mods.LASTMOD > 0: r += "LASTMOD" #?
+	if __mods & mods.KEY9 > 0: r += "K9"
+	if __mods & mods.KEY10 > 0: r += "K10"
+	if __mods & mods.KEY1 > 0: r += "K1"
+	if __mods & mods.KEY3 > 0: r += "K3"
+	if __mods & mods.KEY2 > 0: r += "K2"
+	if __mods & mods.SCOREV2 > 0: r += "SV2(v2)"
+	if __mods & mods.MIRROR > 0: r += "MR"
 	return r
